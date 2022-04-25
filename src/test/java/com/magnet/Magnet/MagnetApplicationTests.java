c4cbe6dc8a2d90e9909ceba6e89e98ab4ebcec45
Mousa Mohammad Abdulmaxod <mousamax101@gmail.com>
2022-04-25 09:47:08 +0200

    Added Spring-Framewrok

---
+++ src/test/java/com/magnet/Magnet/MagnetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magnet.Magnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagnetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
