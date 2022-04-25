38bb6e891d218276ce1567a7cad81cd98251c746
Mousa Mohammad Abdulmaxod <mousamax101@gmail.com>
2022-04-25 12:08:04 +0200

    added Thymeleaf to render HTML templates

--- src/main/java/com/magnet/Magnet/MagnetApplication.java
+++ src/main/java/com/magnet/Magnet/MagnetApplication.java
@@ -3,25 +3,35 @@ package com.magnet.Magnet;
 import org.netpreserve.urlcanon.Canonicalizer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URISyntaxException;
 
 @SpringBootApplication
-@RestController
+@Controller
 public class MagnetApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MagnetApplication.class, args);
 	}
 
-	@GetMapping("/normalize")
-	public String hello(@RequestParam(value = "url", defaultValue = "https://mawdoo3.com/") String url) throws URISyntaxException {
-		url = UrlUtils.normalizeUrl(url);
-		return String.format("<h1>Normalized URL:  <a href=%s>%s</a></h1>", url, url);
+	//home page for the app with search bar
+	@GetMapping("/")
+	public String home() {
+		//return greeting page
+		return "greeting";
 	}
 
-
+	@GetMapping("/normalize")
+	public String hello(@RequestParam(value = "url", defaultValue = "https://mawdoo3.com/") String url , Model model) throws URISyntaxException {
+		url = UrlUtils.normalizeUrl(url);
+		//add model attribute
+		model.addAttribute("url",url);
+		return "result";
+	}
 }
