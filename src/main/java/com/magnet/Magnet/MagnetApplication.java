500085d26acea7c0e272f9aea5712114635ec311
Nader-Youhanna <naderyouhanna@gmail.com>
2022-05-16 13:13:10 +0200

    Began in Results Interface

--- src/main/java/com/magnet/Magnet/MagnetApplication.java
+++ src/main/java/com/magnet/Magnet/MagnetApplication.java
@@ -7,7 +7,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.io.Serializable;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
 
 @SpringBootApplication
 @Controller
@@ -17,29 +19,63 @@ public class MagnetApplication {
 		SpringApplication.run(MagnetApplication.class, args);
 	}
 
-	//home page for the app with search bar
+	// home page for the app with search bar
 	@GetMapping("/")
 	public String home() {
-		//return greeting page
+		// return greeting page
 		return "greeting";
 	}
 
-	//result page from search
+	// result page from search
 	@GetMapping("/result")
-	public String result(@RequestParam(name="query", defaultValue="default") String query, Model model) {
-		//TO DO send query to query processor
-		//query processor return list of urls or filenames
+	public String result(@RequestParam(name = "query", defaultValue = "default") String query, Model model) {
+		// TO DO send query to query processor
+		// query processor return list of urls or filenames
+
+		SearchResult res1 = new SearchResult("https://www.google.com/",
+		"Google", "Google Search Engine");
+	
+		SearchResult res2 = new SearchResult("https://www.youtube.com/",
+		"Youtube", "Youtube");
+
+		SearchResult res3 = new SearchResult("https://www.facebook.com/",
+		"Facebook", "Facebook");
+
+		ArrayList<SearchResult> results = new ArrayList<SearchResult>();
+
+		results.add(res1);
+		results.add(res2);
+		results.add(res3);
+
 		
-		//return result page
+		// return result page
 		model.addAttribute("query", query);
+		model.addAttribute("res", res1);
+		model.addAttribute("size", results.size());
+		model.addAttribute("results", results);
+
 		return "result";
 	}
 
+
 	@GetMapping("/normalize")
-	public String hello(@RequestParam(value = "url", defaultValue = "https://mawdoo3.com/") String url , Model model) throws URISyntaxException {
+	public String hello(@RequestParam(value = "url", defaultValue = "https://mawdoo3.com/") String url, Model model)
+			throws URISyntaxException {
 		url = UrlUtils.normalizeUrl(url);
-		//add model attribute
-		model.addAttribute("url",url);
+		// add model attribute
+		model.addAttribute("url", url);
 		return "result";
 	}
+
 }
+
+class SearchResult {
+	public String url, title, description;
+
+	public SearchResult(String url, String title, String description) {
+		this.url = url;
+		this.title = title;
+		this.description = description;
+	}
+}
+
