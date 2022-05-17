093d4571dc4e52c1b532fb5d3c2e9cf96dd36439
Nader-Youhanna <naderyouhanna@gmail.com>
2022-05-17 18:05:21 +0200

    Finished Indexer

--- src/main/java/com/magnet/Magnet/Indexer.java
+++ src/main/java/com/magnet/Magnet/Indexer.java
@@ -4,6 +4,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.tartarus.snowball.ext.PorterStemmer;
 
 import java.io.*;
@@ -38,25 +40,31 @@ public class Indexer {
                 mp = readHTMLFile(fileName, stopWords, mp);
             }
 
-            writeToFile(convertToJSON(mp).toString(), "./index.json");
+            writeToFile(convertToJSON(mp).toString(), "index.json");
             System.out.println("--------------------------------");
             System.out.println(
                     "\u001B[32m" + "Finished indexing all files\nOutput of the indexer is written in index.json"
                             + "\u001B[0m");
         } else {
             String HTMLFileName;
+            
+            //Read HTML file name from user
             Scanner sc1 = new Scanner(System.in);
             System.out.println("Enter the file name:");
             HTMLFileName = sc1.nextLine();
+            sc1.close();
             
-            sc1.close();
-            String indexFileString = readFile("index.json");
-            sc1.close();
+            //Read the index.json file and save it in a string
+            String indexFileString = readFile("./indexer result/index.json");
+
+            //Parse the json file and save it in a map
             Map<String, Map<String, Double>> mp = parseJSON(indexFileString);
 
-        
+            //Update the map
             mp = readHTMLFile(HTMLFileName, stopWords, mp);
-            writeToFile(convertToJSON(mp).toString(), "./index.json");
+
+            //Write the new index.json
+            writeToFile(convertToJSON(mp).toString(), "index.json");
             System.out.println("--------------------------------");
             System.out.println(
                     "\u001B[32m" + "Finished indexing " + HTMLFileName
@@ -106,15 +114,126 @@ public class Indexer {
     public static Map<String, Map<String, Double>> readHTMLFile(String fileName, Set<String> stopWords,
             Map<String, Map<String, Double>> mp) throws IOException {
 
+        // Define scores for terms in title and in body
+        Double titleScore = 2.0;
+        Double bodyScore = 1.0;
+        // Read file from given filename
+        File input = new File("./html_files/" + fileName);
+        // Use Jsoup to parse the file
+        Document doc = Jsoup.parse(input, "UTF-8", "");
+        // Save the title in a string
+        String title = doc.title();
+        // Save the body text in a string
+        String body = doc.body().text();
+        // Get an array of body words
+        String[] bodyWords = body.split(" ");
+        // Get an array of title words
+        String[] titleWords = title.split(" ");
+        // Calculate docLength, will be used to calculate the normalized TF
+        Integer docLength = bodyWords.length + titleWords.length;
+
+        // Loop on title words and add them to the map
+        for (String word : titleWords) {
+            // Stemming
+            word = stemming(word);
+
+            // Remove stop words
+            if (stopWords.contains(word)) {
+                continue;
+            }
+
+            // Add to map
+
+            // If the map already contains the term
+            if (mp.containsKey(word)) {
+                Map<String, Double> mpTemp = mp.get(word);
+
+                // If the map already contains the filename
+                if (mpTemp.containsKey(fileName)) {
+                    // Normalized TF * docLength = TF
+                    // TF++
+                    // Normalized TF = TF / docLength
+                    Object tf = mpTemp.get(fileName);
+                    Double tf2 = (Double) tf;
+
+                    Double normalizedTF = (Double) (((tf2 * docLength) + titleScore)
+                            / docLength);
+
+                    if (normalizedTF < 0.5f) {
+                        mpTemp.put(fileName, normalizedTF);
+                    } else {
+                        // Spam
+                        mpTemp.put(fileName, 0.0);
+                    }
+                } else {
+                    mpTemp.put(fileName, (Double) (titleScore / docLength));
+                }
+                mp.put(word, mpTemp);
+
+            } else {
+                Map<String, Double> mpTemp = new HashMap<String, Double>();
+                mpTemp.put(fileName, (Double) (titleScore / docLength));
+                mp.put(word, mpTemp);
+            }
+        }
+
+        // Loop on body words and add them to the map
+        for (String word : bodyWords) {
+            // Stemming
+            word = stemming(word);
+
+            // Remove stop words
+            if (stopWords.contains(word)) {
+                continue;
+            }
+
+            // Add to map
+            if (mp.containsKey(word)) {
+                Map<String, Double> mpTemp = mp.get(word);
+
+                if (mpTemp.containsKey(fileName)) {
+                    // Normalized TF * docLength = TF
+                    // TF++
+                    // Normalized TF = TF / docLength
+                    Object tf = mpTemp.get(fileName);
+                    Double tf2 = (Double) tf;
+
+                    Double normalizedTF = (Double) (((tf2 * docLength) + bodyScore)
+                            / docLength);
+
+                    if (normalizedTF < 0.5f) {
+                        mpTemp.put(fileName, normalizedTF);
+                    } else {
+                        // Spam
+                        mpTemp.put(fileName, 0.0);
+                    }
+                } else {
+                    mpTemp.put(fileName, (Double) (bodyScore / docLength));
+                }
+                mp.put(word, mpTemp);
+
+            } else {
+                Map<String, Double> mpTemp = new HashMap<String, Double>();
+                mpTemp.put(fileName, (Double) (bodyScore / docLength));
+                mp.put(word, mpTemp);
+            }
+        }
+
+        return mp;
+    }
+
+    public static Map<String, Map<String, Double>> readHTMLFileOld(String fileName, Set<String> stopWords,
+            Map<String, Map<String, Double>> mp) throws IOException {
+
         // Read HTML file
-        FileReader fr = new FileReader(fileName);
+        FileReader fr = new FileReader("./html_files/" + fileName);
         BufferedReader br = new BufferedReader(fr);
         StringBuilder content = new StringBuilder(1024);
         String s = "";
         // First line is url
-        //String url = br.readLine();
-        //Document document = Jsoup.connect(url).get();
-        //String title = document.title();
+        // String url = br.readLine();
+        // Document document = Jsoup.connect(url).get();
+        // String title = document.title();
         String title = "TEMP";
 
         while ((s = br.readLine()) != null) {
@@ -177,15 +296,14 @@ public class Indexer {
                             // Normalized TF * docLength = TF
                             // TF++
                             // Normalized TF = TF / docLength
-                            
+
                             Object tf = mpTemp.get(fileName);
-                            //Double dblVal = (Double) tf;
+                            // Double dblVal = (Double) tf;
                             Double tf2 = (Double) tf;
-                            
-                            
+
                             Double normalizedTF = (Double) (((tf2 * docLength) + 1.0)
-                            / docLength);
-                           
+                                    / docLength);
+
                             // If the term is in the title, add 0.3
                             if (title.contains(currentWord)) {
                                 normalizedTF += 0.3f;
@@ -255,7 +373,7 @@ public class Indexer {
     }
 
     public static void writeToFile(String str, String filename) throws IOException {
-        BufferedWriter bw = new BufferedWriter(new FileWriter(filename));
+        BufferedWriter bw = new BufferedWriter(new FileWriter("./indexer result/" + filename));
         bw.write(str);
         bw.close();
     }
@@ -263,7 +381,7 @@ public class Indexer {
     public static Set<String> loadStopwords() throws IOException {
 
         // read stopwords from file
-        FileReader fr = new FileReader("./english_stopwords.txt");
+        FileReader fr = new FileReader("./english stopwords/english_stopwords.txt");
         BufferedReader br = new BufferedReader(fr);
         String s = "";
         Set<String> stopwordsSet = new HashSet<String>();
